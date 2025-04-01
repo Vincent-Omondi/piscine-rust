c6b2732e2c3730fb3c5d4867716467e105814f7c
Vincent-Omondi <vincentomondi251@gmail.com>
2025-04-01 18:42:10 +0300

    Update input condition in looping functionality to check for "The letter e" instead of "end"

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         io::stdin().read_line(&mut input).expect("Failed to read line");
         let input = input.trim();
-        if input == "end" {
+        if input == "The letter e" {
             break;
         }
         count += 1;
