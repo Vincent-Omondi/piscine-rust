2a6d935df1f746d6dca76f412bf508bbe630af05
Vincent-Omondi <vincentomondi251@gmail.com>
2025-04-02 21:18:28 +0300

    Refactor to_url function to improve URL encoding by replacing spaces with "%20" using a loop instead of replace method

--- to_url/src/lib.rs
+++ to_url/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn to_url(s: &str) -> String {
-    s.replace(" ", "%20")
+    // s.replace(" ", "%20")
+    let mut url = String::new();
+    for c in s.chars() {
+        if c == ' ' {
+            url.push_str("%20");
+        } else {    
+            url.push(c);
+        }
+    }
+    url
 }
