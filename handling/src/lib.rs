1335a66e808db019a7ce735829e27ca2e786539d
vinomondi <vincentomondi1002@gmail.com>
2025-04-08 11:32:29 +0300

    Change open_or_create function to append content instead of overwriting

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -2,9 +2,9 @@ use std::{fs::OpenOptions, io::Write, path::Path};
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     let mut file = OpenOptions::new()
-        .write(true)
+        .append(true)
         .create(true)
         .open(path)
         .unwrap();
-    write!(file, "{}", content).unwrap();
+    file.write(content.as_bytes()).unwrap();
 }
