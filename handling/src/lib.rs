a8936a7ecf392aeff2fecf67b28e531a2246da0e
vinomondi <vincentomondi1002@gmail.com>
2025-04-08 11:25:17 +0300

    Initialize handling package with Cargo.toml and implement open_or_create function for file operations

---
+++ handling/src/lib.rs
@@ -0,0 +1,10 @@
+use std::{fs::OpenOptions, io::Write, path::Path};
+
+pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(path)
+        .unwrap();
+    writeln!(file, "{}", content).unwrap();
+}
