ca307d2719c4fe8e4f0ffbeaf6a304f9935f342e
Vincent-Omondi <vincentomondi251@gmail.com>
2025-04-10 22:00:59 +0300

    Refactor Member struct and Role enum: consolidate definitions, update constructor, and add promotion functionality

--- mobs/src/mobs/member.rs
+++ mobs/src/mobs/member.rs
@@ -1,24 +1,33 @@
-#[derive(Debug, Clone)]
-pub struct Member {
-    pub name: String,
-    pub age: u8,
-    pub role: Role,
-}
-
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Role {
     Underboss,
-    Caporegime,
     Soldier,
+    Caporegime,
     Associate,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct Member {
+    pub name: String,
+    pub role: Role,
+    pub age: u8,
+}
+
 impl Member {
-    pub fn new(name: String, age: u8) -> Self {
-        Self {
-            name,
-            age,
-            role: Role::Associate,
+    pub fn get_promotion(&mut self) {
+        match self.role {
+            Role::Associate => self.role = Role::Soldier,
+            Role::Soldier => self.role = Role::Caporegime,
+            Role::Caporegime => self.role = Role::Underboss,
+            _ => {}
         }
     }
-} 
+
+    pub fn new(name: &str, role: Role, age: u8) -> Self {
+        Self {
+            name: name.to_string(),
+            role,
+            age,
+        }
+    }
+}
